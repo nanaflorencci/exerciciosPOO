46443c1643164f4db205656321a778009e4aa332
nanaflorencci <giovannaflorencci1814@gmail.com>
2023-09-26 09:26:49 -0300

    .

--- ClasseAbstrata.js
+++ ClasseAbstrata.js
@@ -8,3 +8,36 @@ class Animal {
         console.log("Este é um " + this.nome + "com " + this.patas + "patas.");
     }
 }
+class Dog extends Animal {
+    constructor() {
+        super("Cachorro", 4);
+    }
+    FazerBarulho() {
+        console.log("O Reginaldo está latindo");
+    }
+}
+class Cat extends Animal {
+    constructor() {
+        super("Gato", 4);
+    }
+    FazerBarulho() {
+        console.log("A namorada do Reginaldo está miando");
+    }
+}
+class Bird extends Animal {
+    constructor() {
+        super("Pássaro", 2);
+    }
+    FazerBarulho() {
+        console.log("O primo do Reginaldo está cantando");
+    }
+}
+const cachorro = new Dog();
+cachorro.Apresentar();
+cachorro.FazerBarulho();
+const gato = new Dog();
+gato.Apresentar();
+gato.FazerBarulho();
+const passaro = new Dog();
+passaro.Apresentar();
+passaro.FazerBarulho();
--- ClasseAbstrata.ts
+++ ClasseAbstrata.ts
@@ -13,3 +13,42 @@ abstract class Animal{
         console.log("Este é um " + this.nome + "com " + this.patas + "patas.");
     }
 }
+
+class Dog extends Animal{
+    constructor(){
+        super("Cachorro", 4);
+    }
+    FazerBarulho(): void {
+        console.log("O Reginaldo está latindo");
+    }
+}
+
+class Cat extends Animal{
+    constructor(){
+        super("Gato", 4);
+    }
+    FazerBarulho(): void {
+        console.log("A namorada do Reginaldo está miando");
+    }
+}
+
+class Bird extends Animal{
+    constructor(){
+        super("Pássaro", 2);
+    }
+    FazerBarulho(): void {
+        console.log("O primo do Reginaldo está cantando");
+    }
+}
+
+const cachorro = new Dog();
+cachorro. Apresentar();
+cachorro.FazerBarulho();
+
+const gato = new Dog();
+gato. Apresentar();
+gato.FazerBarulho();
+
+const passaro = new Dog();
+passaro. Apresentar();
+passaro.FazerBarulho();
